2e986308f32a295470b00e555f283c8aa32fe4ae
cann3v <kornilovroman123@gmail.com>
2026-04-21 02:06:31 +0500

    Add health check for rabbit topology

--- Temsa.Core/Configuration/RabbitMqMessagingOptions.cs
+++ Temsa.Core/Configuration/RabbitMqMessagingOptions.cs
@@ -5,4 +5,7 @@ public class RabbitMqMessagingOptions
     public const string SectionName = "RabbitMqMessaging";
     
     public string ScanTasksExchange { get; init; } = string.Empty;
+    public string StaticAnalysisQueue { get; init; } = string.Empty;
+    public string AndroidDynamicAnalysisQueue { get; init; } = string.Empty;
+    public string IosDynamicAnalysisQueue { get; init; } = string.Empty;
 }
--- Temsa.Core/HealthChecks/RabbitMqHealthCheck.cs
+++ Temsa.Core/HealthChecks/RabbitMqHealthCheck.cs
@@ -1,11 +1,18 @@
 using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Options;
+using Temsa.Core.Configuration;
 using Temsa.Core.Infrastructure.Messaging.RabbitMq;
 
 namespace Temsa.Core.HealthChecks;
 
-public class RabbitMqHealthCheck(IRabbitMqConnection rabbitMqConnection): IHealthCheck
+public class RabbitMqHealthCheck(
+    IRabbitMqConnection rabbitMqConnection,
+    IOptions<RabbitMqMessagingOptions> messagingOptions,
+    ILogger<RabbitMqHealthCheck> logger): IHealthCheck
 {
     private readonly IRabbitMqConnection _rabbitMqConnection = rabbitMqConnection;
+    private readonly RabbitMqMessagingOptions _messagingOptions = messagingOptions.Value;
+    private readonly ILogger<RabbitMqHealthCheck> _logger = logger;
 
     public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context,
         CancellationToken cancellationToken = new CancellationToken())
@@ -21,11 +28,27 @@ public class RabbitMqHealthCheck(IRabbitMqConnection rabbitMqConnection): IHealt
 
             await using var channel = await _rabbitMqConnection.CreateChannelAsync(cancellationToken);
 
+            await channel.ExchangeDeclarePassiveAsync(
+                _messagingOptions.ScanTasksExchange,
+                cancellationToken);
+
+            await channel.QueueDeclarePassiveAsync(
+                _messagingOptions.StaticAnalysisQueue,
+                cancellationToken);
+            
+            await channel.QueueDeclarePassiveAsync(
+                _messagingOptions.AndroidDynamicAnalysisQueue,
+                cancellationToken);
+
+            await channel.QueueDeclarePassiveAsync(
+                _messagingOptions.IosDynamicAnalysisQueue,
+                cancellationToken);
+
             return HealthCheckResult.Healthy("RabbitMQ is reachable");
         }
         catch (Exception ex)
         {
-            return HealthCheckResult.Unhealthy("RabbitMQ is unreachable", ex);
+            return HealthCheckResult.Unhealthy("RabbitMQ is unreachable or topology is missing", ex);
         }
     }
 }
